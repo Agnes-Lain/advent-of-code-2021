727f92e881e26b7fa7f5bfcbc8f9659ab69146ad
Yizhu ZONG <agnes.lain@gmail.com>
2021-12-05 08:53:34 +0100

    day 5 complete, very ugly code :(

--- day-03.rb
+++ day-03.rb
@@ -2,19 +2,87 @@ require_relative 'common'
 
 class Day3 < AdventDay
   def first_part
+    a = input.transpose
+    gamma = []
+    epsilon = []
+    a.each do | s |
+      one = s.count("1")
+      zero = s.count("0")
+      if one > zero
+        gamma << "1"
+        epsilon << "0"
+      else
+        gamma << "0"
+        epsilon << "1"
+      end
+    end
+    gamma_f = to_decimal(gamma.join)
+    epsilon_f = to_decimal(epsilon.join)
+    gamma_f * epsilon_f
   end
 
   def second_part
+    oxygen = input
+    co2 = input
+    12.times do |t|
+      if oxygen.size > 1
+        # binding.pry
+        o_a = oxygen.transpose
+        if o_a[t].count("1") >= o_a[t].count("0")
+          oxygen = oxygen.select {|a| a[t] == "1"}
+        else
+          oxygen = oxygen.select {|a| a[t] == "0"}
+        end
+      end
+    end
+
+    12.times do |t|
+      if co2.size > 1
+        c_a = co2.transpose
+        if c_a[t].count("0") <= c_a[t].count("1")
+          co2 = co2.select {|a| a[t]=="0"}
+        else
+          co2 = co2.select {|a| a[t] == "1"}
+        end
+      end
+    end
+    o = to_decimal(oxygen.flatten.join())
+    c = to_decimal(co2.flatten.join())
+    o * c
   end
 
   private
 
   def convert_data(data)
-    super
+    super.flatten.map {|s| s.split("")}
+  end
+
+  def to_decimal(binary)
+    raise ArgumentError if binary.match?(/[^01]/)
+
+    binary.reverse.chars.map.with_index do |digit, index|
+      digit.to_i * 2**index
+    end.sum
+  end
+
+  def selector(array, s_num, s_num2)
+    arr = array
+    arr[0].size .times do |t|
+      if arr.size > 1
+        c_a = arr.transpose
+        if c_a[t].count(s_num) <= c_a[t].count(s_num2)
+          arr = arr.select {|a| a[t]==s_num}
+        else
+          arr = arr.select {|a| a[t] == s_num2}
+        end
+      end
+    end
+    arr
   end
 
   def debug_input
-    "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2"
+    "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010"
+
   end
 end
 
--- day-05.rb
+++ day-05.rb
@@ -1,20 +1,97 @@
 require_relative 'common'
+require "byebug"
 
 class Day5 < AdventDay
   def first_part
+    lines = input.select {|c| c[0][0] == c[1][0] || c[0][1] == c[1][1]}
+    hash = {}
+    lines.each do |points|
+      x1 = points[0][0]
+      y1 = points[0][1]
+      x2 = points[1][0]
+      y2 = points[1][1]
+      if x1 == x2
+        min = [y1, y2].min
+        max = [y1, y2].max
+        (min..max).each do |y|
+          k = [x1, y]
+          hash[k] ? hash[k] += 1 : hash[k] = 1
+        end
+      else
+        min = [x1, x2].min
+        max = [x1, x2].max
+        (min..max).each do |x|
+          k = [x, y1]
+          hash[k] ? hash[k] += 1 : hash[k] = 1
+        end
+      end
+    end
+    final = hash.select { |_k,v|  v >= 2 }
+    final.size
   end
 
   def second_part
+    # lines = input.select {|c| c[0][0] == c[1][0] || c[0][1] == c[1][1]|| (c[0][0]-c[1][0]).abs == (c[0][1]-c[1][1]).abs }
+    hash = {}
+    input.each do |points|
+      x1 = points[0][0]
+      y1 = points[0][1]
+      x2 = points[1][0]
+      y2 = points[1][1]
+      if x1 == x2
+        min = [y1, y2].min
+        max = [y1, y2].max
+        (min..max).each do |y|
+          k = [x1, y]
+          hash[k] ? hash[k] += 1 : hash[k] = 1
+        end
+      elsif y1 == y2
+        min = [x1, x2].min
+        max = [x1, x2].max
+        (min..max).each do |x|
+          k = [x, y1]
+          hash[k] ? hash[k] += 1 : hash[k] = 1
+        end
+      else
+        x_min = [x1, x2].min
+        x_max = [x1, x2].max
+        y_min = [y1, y2].min
+        y_max = [y1, y2].max
+        if x_min == x1 && y_min == y1
+          (x_min..x_max).each do |x|
+            # byebug
+            k = [x, y_min]
+            hash[k] ? hash[k] += 1 : hash[k] = 1
+            y_min += 1
+          end
+        elsif x_max == x1 && y_max == y1
+          (x_min..x_max).each do |x|
+            k = [x, y_min]
+            hash[k] ? hash[k] += 1 : hash[k] = 1
+            y_min += 1
+          end
+        else
+          (x_min..x_max).each do |x|
+            k = [x, y_max]
+            hash[k] ? hash[k] += 1 : hash[k] = 1
+            y_max -= 1
+          end
+        end
+      end
+    end
+    final = hash.select { |_k,v|  v >= 2 }
+    final.size
+    # hash
   end
 
   private
 
   def convert_data(data)
-    super
+    super.map {|s| s.split(" -> ").map{|c| c.split(",").map{|e| e.to_i }}}
   end
 
   def debug_input
-    "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2"
+    "0,9 -> 5,9\n8,0 -> 0,8\n9,4 -> 3,4\n2,2 -> 2,1\n7,0 -> 7,4\n6,4 -> 2,0\n0,9 -> 2,9\n3,4 -> 1,4\n0,0 -> 8,8\n5,5 -> 8,2"
   end
 end
 
