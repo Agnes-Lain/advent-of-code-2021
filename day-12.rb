94be96f5e81182308ed036c2cfe8b95475b88282
Yizhu ZONG <agnes.lain@gmail.com>
2021-12-13 22:15:43 +0100

    pround of my matrix solution of day 13

--- day-12.rb
+++ day-12.rb
@@ -1,20 +1,93 @@
 require_relative 'common'
+require "byebug"
 
 class Day12 < AdventDay
   def first_part
+    hash = input
+    paths = []
+    current_visited = []
+    current_path = []
+    find_node("start", ["start"], [], hash, paths)
+    paths.size
   end
 
   def second_part
+    hash = input
+    paths = []
+    current_visited = []
+    current_path = []
+    small_caves = hash.keys.select{|k| k != "start" && k != "end" && /[a-z]/.match?(k) }
+    small_caves.each {|small_cave| find_next_node([small_cave, 0], "start", ["start"], [], hash, paths)}
+    results = paths.map {|p| p.join(",")}
+    uniques = results.uniq.sort
+    uniques.size
   end
 
   private
 
+  def find_node(node, current_path, current_visited, hash, paths)
+    # puts "#{node}, #{current_path}, #{current_visited}"
+    hash[node].each do |dest|
+      # byebug
+      if /[a-z]/.match?(dest)
+        if current_visited.include?(dest)
+          # bad path, ignore
+            next
+        end
+      end
+      new_path = current_path + [dest]
+      new_visited = current_visited + [node]
+
+      if dest == "end"
+        paths.append(new_path)
+      else
+        find_node(dest, new_path, new_visited, hash, paths)
+      end
+    end
+  end
+
+  def find_next_node(small_cave, node, current_path, current_visited, hash, paths)
+    # puts "#{node}, #{current_path}, #{current_visited}"
+    hash[node].each do |dest|
+      new_small_cave = [small_cave[0], small_cave[1]]
+      # byebug
+      if /[a-z]/.match?(dest)
+        if current_visited.include?(dest)
+          if dest == new_small_cave[0]
+            if new_small_cave[1] == 1
+              # bad path, ignore
+              next
+            else
+              new_small_cave[1] += 1
+            end
+          else
+            next
+          end
+        end
+      end
+      new_path = current_path + [dest]
+      new_visited = current_visited + [node]
+
+      if dest == "end"
+        paths.append(new_path)
+      else
+        find_next_node(new_small_cave, dest, new_path, new_visited, hash, paths)
+      end
+    end
+  end
+
   def convert_data(data)
-    super
+    hash = {}
+    super.map {|pair| pair.split("-")}.each do |pair|
+      hash[pair[0]] ? hash[pair[0]] << pair[1] : hash[pair[0]] = [pair[1]]
+      hash[pair[1]] ? hash[pair[1]] << pair[0] : hash[pair[1]] = [pair[0]]
+    end
+    hash
   end
 
   def debug_input
-    "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2"
+    # "start-A\nstart-b\nA-c\nA-b\nb-d\nA-end\nb-end"
+    "dc-end\nHN-start\nstart-kj\ndc-start\ndc-HN\nLN-dc\nHN-end\nkj-sa\nkj-HN\nkj-dc"
   end
 
 end
--- day-13.rb
+++ day-13.rb
@@ -1,20 +1,73 @@
 require_relative 'common'
+require "matrix"
+require "byebug"
 
 class Day13 < AdventDay
   def first_part
+    i = input.index("")
+    dots = input[0...i].map{|pair| pair.split(",").map{|e| e.to_i}}
+    x_max = dots.map{|a| a[0]}.max
+    y_max = dots.map{|a| a[1]}.max
+    folds = input[(i + 1)...input.size].map{|e| e.split(" ")[2].split("=")}
+    board = Matrix.zero(y_max+1, x_max+1)
+    dots.each do |dot|
+      board[dot[1],dot[0]] = 1
+    end
+    board = board.to_a
+    new_board = fold(board, folds[0][0], folds[0][1])
+    # new_board = fold(new_board, folds[1][0], folds[1][1])
+    new_board.map{|arr| arr.sum }.sum
+    # new_board
   end
 
+
   def second_part
+    i = input.index("")
+    dots = input[0...i].map{|pair| pair.split(",").map{|e| e.to_i}}
+    x_max = dots.map{|a| a[0]}.max
+    y_max = dots.map{|a| a[1]}.max+2
+    folds = input[(i + 1)...input.size].map{|e| e.split(" ")[2].split("=")}
+    board = Matrix.zero(y_max+1, x_max+1)
+    dots.each do |dot|
+      board[dot[1],dot[0]] = 1
+    end
+    board = board.to_a
+    # board = fold(board, folds[0][0], folds[0][1])
+    # byebug
+    # board = fold(board, folds[1][0], folds[1][1])
+    folds.each do |f|
+      # byebug
+      board = fold(board, f[0], f[1])
+    end
+    board
   end
 
   private
+
+  def fold(board, dir, num)
+    num = num.to_i
+    if dir == "y"
+      # byebug
+      new_board = Matrix[*board[0...num]]
+      board2 = Matrix[*board[(num+1)...board.size].reverse]
+      new_board += board2
+      return new_board.to_a.map{|arr| arr.map{|e| e > 1 ? e = 1 : e}}
+    else
+      trans_board = board.transpose
+      new_board = Matrix[*trans_board[0...num]]
+      board2 = Matrix[*trans_board[(num+1)...trans_board.size].reverse]
+      new_board += board2
+      new_board = new_board.transpose.to_a
+      return new_board.map{|arr| arr.map{|e| e > 1 ? e = 1 : e}}
+    end
+  end
 
   def convert_data(data)
     super
   end
 
   def debug_input
-    "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2"
+    "6,10\n0,14\n9,10\n0,3\n10,4\n4,11\n6,0\n6,12\n4,1\n0,13\n10,12\n3,4\n3,0\n8,4\n1,10\n2,14\n8,10\n9,0\n\nfold along y=7\nfold along x=5"
   end
 
 end
